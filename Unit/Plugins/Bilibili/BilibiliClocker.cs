34cc383c7c5520857bc4ddbaf6066e20a35827d6
ReturnNefe <ret-chen@outlook.com>
2023-04-04 10:50:39 +0800

    Add BilibiliClocker

---
+++ Unit/Plugins/Bilibili/BilibiliClocker.cs
@@ -0,0 +1,29 @@
+﻿using AutoClock.Interface;
+
+namespace Bilibili
+{
+    public class BilibiliClocker : IClocker
+    {
+        // Private Properties
+        private PluginInfo info = new("Bilibili", "ReturnNefe", "A clocker for Bilibili.", new("0.1.0"));
+        private DateTime final = DateTime.Now;
+        
+        // Public Properties
+        public PluginInfo Info => info;
+
+        public Task<(bool, string)> Clock()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Loading(string baseDirectory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Wait()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
